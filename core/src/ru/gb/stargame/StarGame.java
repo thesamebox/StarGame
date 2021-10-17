9126f62ec691d9c6100c78ec01fe9ff88d3c7004
iddqd <simondo@inbox.ru>
2021-10-17 13:51:33 +0300

    hometask1

--- core/src/ru/gb/stargame/StarGame.java
+++ core/src/ru/gb/stargame/StarGame.java
@@ -1,26 +1,42 @@
 package ru.gb.stargame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	int x;
+	int y;
+	TextureRegion region;
+	Texture background;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		region = new TextureRegion(img, 20, 25, 100, 50);
+		background = new Texture("background.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 1, 0, 1);
+		ScreenUtils.clear(Color.MAROON);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0,0);
+		batch.setColor(1,1,1, 0.5f);
+		batch.draw(img, x, y, 100, 100);
+		batch.setColor(1, 0.1f, 0.9f, 1f);
+		batch.draw(region, 200, 100);
+		batch.setColor(1,1,1,0.5f);
+		batch.draw(region, 100, 300);
 		batch.end();
+		x=x+2;
+		y++;
 	}
 	
 	@Override
